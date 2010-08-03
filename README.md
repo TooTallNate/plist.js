d176df56387994f2605065e0674e0d2900940829
Nathan Rajlich <nathan@tootallnate.net>
2010-08-03 01:53:00 -0700

    Sloppy initial commit. Have it working up to iTunes XML files currently.

---
+++ README.md
@@ -0,0 +1,7 @@
+node-plist
+==========
+
+This library contains a parser for Mac OS X Plist (property list) files. These
+are often used in programming OS X and iOS applications, as well as the iTunes
+configuration XML file.
+
---
+++ tests/test.js
@@ -0,0 +1,28 @@
+var sys = require("sys");
+var path = require("path");
+var assert = require("assert");
+var plist = require("../lib/node-plist");
+
+// The Plist file to parse...
+var file = path.join(__dirname, "iTunes-BIG.xml");
+
+var startTime = new Date();
+
+plist.parseFile(file, function(err, dicts) {
+
+  if (err) {
+    throw err;
+  }
+
+  var endTime = new Date();
+  sys.puts('Parsed "' + file + '" in ' + (endTime - startTime) + 'ms');
+
+  //sys.puts(sys.inspect(dicts));
+  //sys.puts(JSON.stringify(dicts));
+  
+  var dict = dicts[0];
+  assert.equal(dict['Application Version'], "9.0.3");
+  assert.equal(dict['Library Persistent ID'], "6F81D37F95101437");
+  
+});
+
