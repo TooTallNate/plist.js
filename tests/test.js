7283ca94c1cc4f1e33e6bbf411dae3157a4c3752
Nathan Rajlich <nathan@tootallnate.net>
2011-05-05 22:58:57 -0700

    Better test case. Replace `sys.puts()` with `console.log()`.

--- tests/test.js
+++ tests/test.js
@@ -1,39 +1,39 @@
-var sys = require("sys");
 var path = require("path");
 var assert = require("assert");
-var plist = require("../lib/plist");
+var plist = require("../");
 
-// The Plist file to parse...
+// First test...
 var file = path.join(__dirname, "iTunes-small.xml");
-
-var startTime = new Date();
+var startTime1 = new Date();
 
 plist.parseFile(file, function(err, dicts) {
-
   if (err) {
     throw err;
   }
 
   var endTime = new Date();
-  sys.puts('Parsed "' + file + '" in ' + (endTime - startTime) + 'ms');
+  console.log('Parsed "' + file + '" in ' + (endTime - startTime1) + 'ms');
 
-  sys.puts(sys.inspect(dicts, true, 5));
-  //sys.puts(JSON.stringify(dicts));
-  
   var dict = dicts[0];
-  //assert.equal(dict['Application Version'], "9.0.3");
-  //assert.equal(dict['Library Persistent ID'], "6F81D37F95101437");
-  
+  assert.equal(dict['Application Version'], "9.0.3");
+  assert.equal(dict['Library Persistent ID'], "6F81D37F95101437");
+
 });
 
-var plistFile = path.join(__dirname, "tests/sample2.plist");
 
-plist.parseFile(plistFile, function(err, dicts) {
+// Second tests. Runs concurrently with the first test...
+var file2 = path.join(__dirname, "sample2.plist");
+var startTime2 = new Date();
+
+plist.parseFile(file2, function(err, dicts) {
   if (err) {
     throw err;
   }
 
-  var dict = dicts[0];
+  var endTime = new Date();
+  console.log('Parsed "' + file2 + '" in ' + (endTime - startTime2) + 'ms');
 
+  var dict = dicts[0];
   assert.equal(dict['PopupMenu'][2]['Key'], "\n        #import &lt;Cocoa/Cocoa.h&gt;\n\n#import &lt;MacRuby/MacRuby.h&gt;\n\nint main(int argc, char *argv[])\n{\n  return macruby_main(\"rb_main.rb\", argc, argv);\n}\n");
+
 });
