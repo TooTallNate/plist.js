9bf486fc683c9be814c2987e5a3f10222c003a3e
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-01-17 16:35:08 +0100

    Add Xcode plist tests + stringify function

--- tests/test.js
+++ tests/test.js
@@ -18,6 +18,11 @@ plist.parseFile(file, function(err, dicts) {
   assert.equal(dict['Application Version'], "9.0.3");
   assert.equal(dict['Library Persistent ID'], "6F81D37F95101437");
 
+  // Try re-stringifying and re-parsing
+  plist.parseString(plist.stringify(dicts), function(err, dicts2) {
+    assert.deepEqual(dicts,dicts2);
+  });
+  
 });
 
 
@@ -35,6 +40,11 @@ plist.parseFile(file2, function(err, dicts) {
 
   var dict = dicts[0];
   assert.equal(dict['PopupMenu'][2]['Key'], "\n        \n        #import &lt;Cocoa/Cocoa.h&gt;\n\n#import &lt;MacRuby/MacRuby.h&gt;\n\nint main(int argc, char *argv[])\n{\n  return macruby_main(\"rb_main.rb\", argc, argv);\n}\n\n");
+
+  // Try re-stringifying and re-parsing
+  plist.parseString(plist.stringify(dicts), function(err, dicts2) {
+    assert.deepEqual(dicts,dicts2);
+  });
 
 });
 
@@ -54,5 +64,58 @@ plist.parseFile(file3, function(err, dicts) {
   var dict = dicts[0];
   assert.equal(dict['duration'], 5555.0495000000001);
   assert.equal(dict['position'], 4.6269989039999997);
+
+  // Try re-stringifying and re-parsing
+  plist.parseString(plist.stringify(dicts), function(err, dicts2) {
+    assert.deepEqual(dicts,dicts2);
+  });
+
+});
+
+// Fourth test...
+var file4 = path.join(__dirname, "Xcode-Info.plist");
+var startTime4 = new Date();
+
+plist.parseFile(file4, function(err, dicts) {
+  if (err) {
+    throw err;
+  }
+
+  var endTime = new Date();
+  console.log('Parsed "' + file4 + '" in ' + (endTime - startTime4) + 'ms');
+
+  var dict = dicts[0];
+  assert.equal(dict['CFBundleAllowMixedLocalizations'], true);
+  assert.equal(dict['CFBundleExecutable'], "${EXECUTABLE_NAME}");
+  assert.equal(dict['UISupportedInterfaceOrientations~ipad'][0], "UIInterfaceOrientationPortrait");
+
+  // Try re-stringifying and re-parsing
+  plist.parseString(plist.stringify(dicts), function(err, dicts2) {
+    assert.deepEqual(dicts,dicts2);
+  });
+
+});
+
+
+// Fifth test...
+var file5 = path.join(__dirname, "Xcode-PhoneGap.plist");
+var startTime5 = new Date();
+
+plist.parseFile(file5, function(err, dicts) {
+  if (err) {
+    throw err;
+  }
+
+  var endTime = new Date();
+  console.log('Parsed "' + file5 + '" in ' + (endTime - startTime5) + 'ms');
+
+  var dict = dicts[0];
+  assert.equal(dict['ExternalHosts'][0], "*");
+  assert.equal(dict['Plugins']['com.phonegap.accelerometer'], "PGAccelerometer");
+  
+  // Try re-stringifying and re-parsing
+  plist.parseString(plist.stringify(dicts), function(err, dicts2) {
+    assert.deepEqual(dicts,dicts2);
+  });
 
 });
