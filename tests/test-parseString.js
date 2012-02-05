da3ac9aa431ccbb0e5a95d57d2d68bab5d347210
Pierre Metrailler <pierre@spotme.com>
2012-02-05 14:05:37 +0100

    Implemented support for real, identity tests

--- tests/test-parseString.js
+++ tests/test-parseString.js
@@ -20,7 +20,15 @@ plist.parseString("<plist><dict><key>test</key><integer>101</integer></dict></pl
   assert.equal(res[0].test, 101);
 });
 
+var gotResponse3 = false;
+plist.parseString("<string><![CDATA[Hello World!&lt;M]]></string>", function(err, res) {
+  if (err) throw err;
+  gotResponse3 = true;
+  assert.equal(res, 'Hello World!&lt;M');
+});
 
 process.on('exit', function() {
-  assert.ok(gotResponse1);
+    assert.ok(gotResponse1);
+    assert.ok(gotResponse2);
+    assert.ok(gotResponse3);
 });
