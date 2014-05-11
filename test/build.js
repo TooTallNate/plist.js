ad4fb5d76be2ac1b1f34c54bd44ffed9a362718e
Nathan Rajlich <nathan@tootallnate.net>
2014-05-11 00:05:22 -0700

    test: add some more build() tests

--- test/build.js
+++ test/build.js
@@ -40,6 +40,66 @@ describe('plist', function () {
 */}));
     });
 
+    it('should create a plist XML date from a Date', function () {
+      var xml = build(new Date('2010-02-08T21:41:23Z'));
+      assert.strictEqual(xml, multiline(function () {/*
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <date>2010-02-08T21:41:23Z</date>
+</plist>
+*/}));
+    });
+
+    it('should create a plist XML date from a Buffer', function () {
+      var xml = build(new Buffer('☃'));
+      assert.strictEqual(xml, multiline(function () {/*
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <data>4piD</data>
+</plist>
+*/}));
+    });
+
+    it('should create a plist XML true from a `true` Boolean', function () {
+      var xml = build(true);
+      assert.strictEqual(xml, multiline(function () {/*
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <true/>
+</plist>
+*/}));
+    });
+
+    it('should create a plist XML false from a `false` Boolean', function () {
+      var xml = build(false);
+      assert.strictEqual(xml, multiline(function () {/*
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <false/>
+</plist>
+*/}));
+    });
+
+    it('should create a plist XML array from an Array', function () {
+      var xml = build([ 1, 'foo', false, new Date(1234) ]);
+      assert.strictEqual(xml, multiline(function () {/*
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <array>
+    <integer>1</integer>
+    <string>foo</string>
+    <false/>
+    <date>1970-01-01T00:00:01Z</date>
+  </array>
+</plist>
+*/}));
+    });
+
   });
 
 });
