5ed1362cbb8e2be2d33ee4a6c07512988f75f065
Dave Nicolson <david.nicolson@gmail.com>
2019-11-19 02:26:56 +0100

    Fix Buffer deprecation warning

--- test/build.js
+++ test/build.js
@@ -52,7 +52,7 @@ describe('plist', function () {
     });
 
     it('should create a plist XML date from a Buffer', function () {
-      var xml = build(new Buffer('☃'));
+      var xml = build(new Buffer.from('☃'));
       assert.strictEqual(xml, multiline(function () {/*
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
